a6e0212200e289eefd8032aed874702c3f6e4273
BarkerProoks <jonparkerbrooks@gmail.com>
2025-05-13 20:27:36 -0500

    added viper slide code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpModeClaw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpModeClaw.java
@@ -22,9 +22,6 @@ public class TeleOpModeClaw extends LinearOpMode {
     final int ARM_STRAIGHT_UP   = (int) (120 * ARM_TPD);
     final int ARM_WINCH_ROBOT   = (int) (15 * ARM_TPD);
 
-    // wrist pre-defined constants
-    final double WRIST_FOLDED_IN  = 0.85;
-    final double WRIST_FOLDED_OUT = 0.5;
 
     final double CLAW_OPEN = 0.68;
     final double CLAW_CLOSED = 0.98;
@@ -55,22 +52,19 @@ public class TeleOpModeClaw extends LinearOpMode {
         return claw;
     }
 
-    private Servo setupWristServo(String label) {
-        Servo wrist = hardwareMap.get(Servo.class, label);
-        wrist.resetDeviceConfigurationForOpMode();
-        wrist.setPosition(WRIST_FOLDED_IN);
-        return wrist;
-    }
-
     @Override
     public void runOpMode() throws InterruptedException {
         DcMotor lDrive = setupDriveMotor("l_drive", DcMotorSimple.Direction.REVERSE);
         DcMotor rDrive = setupDriveMotor("r_drive", DcMotorSimple.Direction.FORWARD);
+
+        DcMotor slide = setupArmMotor("slide");
         DcMotor arm = setupArmMotor("arm");
 
         Servo claw = setupClawServo("claw");
-        Servo wrist = setupWristServo("wrist");
 
+        boolean slideExtended = false;
+
+        int slidePositiion = 0;
         int armPosition = ARM_INITIAL;
 
         telemetry.addLine("Waiting for robot to start...");
@@ -92,58 +86,62 @@ public class TeleOpModeClaw extends LinearOpMode {
             lDrive.setPower(l / 1.5);
             rDrive.setPower(r / 1.5);
 
-            // control wrist
-            if (gamepad1.right_trigger > 0.5) // RT -> fold wrist in
+            if (gamepad1.right_trigger > 0.5) {
                 armPosition += 1;
-            if (gamepad1.left_trigger > 0.5) // LT -> fold wrist out
+            } else if (gamepad1.left_trigger > 0.5) {
                 armPosition -= 1;
+            }
 
+            // open and close the claw
             if (gamepad1.a)
-                    claw.setPosition(CLAW_CLOSED);
+                claw.setPosition(CLAW_CLOSED);
             if (gamepad1.b)
-                    claw.setPosition(CLAW_OPEN);
+                claw.setPosition(CLAW_OPEN);
+
+            // extend or retract the slide
+            if (gamepad1.x)
+                slideExtended = !slideExtended;
 
             // arm position mappings
             if (gamepad1.y) // Y -> Move arm to score in the basket
                 armPosition = ARM_SCORE_SAMPLES;
+
             if (gamepad1.left_bumper) // LB -> Move arm to clear barrier
                 armPosition = ARM_CLEAR_BARRIER;
+            if (gamepad1.right_bumper) // RB -> Move arm to intake position and turn on intake
+                armPosition = ARM_COLLECT;
+
+            // controls for arm winching
             if (gamepad1.dpad_up) // D-Up -> prepare arm for hanging
                 armPosition = ARM_STRAIGHT_UP;
             if (gamepad1.dpad_down) // D-Down -> winch robot upwards
                 armPosition = ARM_WINCH_ROBOT;
-
-            if (gamepad1.right_bumper) { // RB -> Move arm to intake position and turn on intake
-                wrist.setPosition(WRIST_FOLDED_OUT);
-                armPosition = ARM_COLLECT;
-            }
-
-            if (gamepad1.dpad_left) { // D-Left -> Reset arm to original position
-                wrist.setPosition(WRIST_FOLDED_IN);
+            if (gamepad1.dpad_left) // D-Left -> Reset arm to original position
                 armPosition = ARM_INITIAL;
-            }
-
-            if (gamepad1.dpad_right) { // D-Right -> Set arm to specimen score configuration
-                wrist.setPosition(0.0);
+            if (gamepad1.dpad_right) // D-Right -> Set arm to specimen score configuration
                 armPosition = ARM_SCORE_SAMPLES;
-            }
 
             // set target position
             arm.setTargetPosition(armPosition);
             arm.setPower(1.0); // go at full power!
             arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
+            slide.setTargetPosition(slideExtended ? -1650 : 0);
+            slide.setPower(1.0);
+            slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
             // send relevant data to driver station
             telemetry.addLine("L Analog: Forward / Backward");
             telemetry.addLine("R Analog: Left / Right");
             telemetry.addLine();
-            telemetry.addLine("R Bumper: Feed Mode");
+            telemetry.addLine("R Bumper: Claw grab mode");
             telemetry.addLine("L Bumper: Clear Lower Barrier");
             telemetry.addLine("Y: Score Mode");
             telemetry.addLine();
-            telemetry.addLine("A: Intake Pull");
-            telemetry.addLine("B: Intake Push");
-            telemetry.addLine("X: Intake Off");
+            telemetry.addLine("A: Claw open");
+            telemetry.addLine("B: Claw close");
+            telemetry.addLine();
+            telemetry.addLine("X: Move slide in / out");
             telemetry.addLine();
             telemetry.addLine("DPad Right: Home Mode");
             telemetry.addLine();
@@ -151,9 +149,8 @@ public class TeleOpModeClaw extends LinearOpMode {
             telemetry.addLine();
             telemetry.addData("Right Motor", rDrive.getPower());
             telemetry.addData(" Left Motor", lDrive.getPower());
+            telemetry.addData("Slide Motor", slideExtended ? "extended" : "retracted");
             telemetry.addData("        Arm", armPosition);
-            telemetry.addData("      Wrist", wrist.getPosition());
-            telemetry.addData("     Intake", claw.getPosition());
             telemetry.update();
         }
     }
